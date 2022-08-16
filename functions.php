d8efa3b564c209d9dd337f58a93fe2cb643493bb
Jorge Parra <jorgeparraandrade@gmail.com>
2022-08-16 15:51:46 -0400

    Theme version, small edits

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
+  $THEME_VERSION = $NPM_PACKAGE['version'];
+
+  define( 'THEME_VERSION', $THEME_VERSION );
 
   add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 11 );
   function enqueue_child_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/dist/theme.css' );
-    wp_enqueue_style( 'child-style-map', get_stylesheet_directory_uri().'/dist/theme.css.map' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/dist/theme.css',array(), THEME_VERSION );
+    wp_enqueue_style( 'child-style-map', get_stylesheet_directory_uri().'/dist/theme.css.map',array(), THEME_VERSION );
     wp_register_script( 'hashedjs', get_stylesheet_directory_uri() . '/dist/theme.js', array('jquery'),'', false );
     wp_enqueue_script('hashedjs');
   }
