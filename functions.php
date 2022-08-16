7ae10c3c4cc7404bd0a844ac048c09241499d116
Jorge Parra <jorgeparraandrade@gmail.com>
2022-08-16 16:38:46 -0400

    Dequeue parent theme js file

--- functions.php
+++ functions.php
@@ -5,8 +5,11 @@
 
   add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 11 );
   function enqueue_child_styles() {
+
+    wp_dequeue_script( 'prelude-js' );
+    wp_deregister_script( 'prelude-js' );
+
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/dist/theme.css',array(), THEME_VERSION );
     wp_enqueue_style( 'child-style-map', get_stylesheet_directory_uri().'/dist/theme.css.map',array(), THEME_VERSION );
-    wp_register_script( 'hashedjs', get_stylesheet_directory_uri() . '/dist/theme.js', array('jquery'),'', false );
-    wp_enqueue_script('hashedjs');
+    wp_enqueue_script( 'childprelude-js', get_stylesheet_directory_uri() . '/dist/theme.js', array('jquery'),'', false );
   }
